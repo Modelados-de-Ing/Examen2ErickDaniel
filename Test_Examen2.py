c5e1fc8c298069de0e0535685d22286b013dfe10
Daniel Vargas <danieljvargas@estudiantec.cr>
2023-11-13 00:56:00 -0600

    Agregué mis cuatro pruebas.

--- Test_Examen2.py
+++ Test_Examen2.py
@@ -1,6 +1,6 @@
 from Examen2 import MiClase
 import unittest
-class TestDB(unittest.TestCase):
+class test(unittest.TestCase):
     def test_ObtieneValencia (self):
          db = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
          self.assertEqual(db.ObtieneValencia(123589),4)
@@ -13,5 +13,22 @@ class TestDB(unittest.TestCase):
     def test_VerificaListaCanciones(self):
         db= MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
         self.assertTrue(db.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]))
+
+    def test_ObtieneValencia (self):
+        db = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(db.ObtieneValencia(5790238172),6)
+
+    def test_ObtieneMasBailable(self):
+        db = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(db.ObtieneMasBailable([25,245,18]),245)
+
+    def test_VerificaListaCanciones(self):
+        db= MiClase(5, 120, 12, ["Canción 1", "Canción 5", "Canción 9"], [0.8, 0.9, 0.7])
+        self.assertTrue(db.VerificaListaCanciones(["Canción 2", "Canción 5", "Canción 9"]))
+
+    def test_DivisibleTempo(self):
+        db = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+        self.assertEqual(db.DivisibleTempo(5),[1,5])
+
 if __name__ == "__main__":
     unittest.main()
